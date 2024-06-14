faf2e0a046c3d159ddd98c27b5471ce705fe289b
Francisco <160526928+RodriguezSanchezFranciscoAlejandro@users.noreply.github.com>
2024-06-14 01:49:40 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+inicio
+imprimir Francisco Alejandro Rodriguez Sanchez
+imprimir colonia: quesadilla 
+imprimir municipio: melocoton
+imprimir telefono: 844 888 8888
+fin
---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Francisco Alejandro Rodriguez Sanchez</p>'; ?> 
+    <?php echo '<p>colonia: quesadilla</p>'; ?> 
+    <?php echo '<p>municipio: melocoton</p>'; ?> 
+    <?php echo '<p>telefono: 844 888 8888</p>'; ?> 
+</html>
